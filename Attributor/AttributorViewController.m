8c0b2f177eff0b6d76766ea9d0a021fd2b422304
Dave Carpenter <dave@ungarst.com>
2014-04-14 13:09:34 +1200

    Make changes from the Controller Life Cycle lecture which make the user interface responsive to font size changes

--- Attributor/AttributorViewController.m
+++ Attributor/AttributorViewController.m
@@ -44,6 +44,34 @@
                                    range:self.body.selectedRange];
 }
 
+- (void) viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self usePreferredFonts];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(preferredFontsChanged:)
+                                                 name:UIContentSizeCategoryDidChangeNotification
+                                               object:nil];
+}
+
+- (void) preferredFontsChanged:(NSNotification *) notification
+{
+    [self usePreferredFonts];
+}
+
+- (void) usePreferredFonts
+{
+    self.body.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    self.headline.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+}
+
+- (void) viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIContentSizeCategoryDidChangeNotification
+                                                  object:nil];
+}
 
 - (IBAction)unoutlineBodySelection:(id)sender {
     [self.body.textStorage removeAttribute:NSStrokeWidthAttributeName
